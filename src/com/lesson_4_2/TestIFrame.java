f6742d34e0b3136b7ae91868628dff94a743e046
OlgaAsadchaya <asadch@tut.by>
2019-03-14 23:04:00 +0300

    Changes after review

--- src/com/lesson_4_2/TestIFrame.java
+++ src/com/lesson_4_2/TestIFrame.java
@@ -1,39 +1,29 @@
 package com.lesson_4_2;
 
-import org.junit.*;
 
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.After;
-
+import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-//import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestIFrame {
     WebDriver driver;
 
-    @org.junit.Before
-    public void setUp() throws Exception {
+    @Before
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/iframe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
     }
 
-    @org.junit.After
-    public void tearDown() throws Exception {
+    @After
+    public void tearDown() {
         driver.close();
     }
     @Test
